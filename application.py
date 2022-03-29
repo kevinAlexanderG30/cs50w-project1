a677755d536acc41937e9b3162f20cd500e38291
Kevin Guadamuz <kevin.guadamuz>
2022-03-29 16:01:56 -0600

    se empezo a trabajar en register y login

--- application.py
+++ application.py
@@ -1,19 +1,20 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, redirect, render_template, request
 from flask_session import Session
+from tempfile import mkdtemp
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import  login_required
+from helpers import login_required
 
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+#    raise RuntimeError("DATABASE_URL is not set")
 
 # Ensure responses aren't cached
 @app.after_request
@@ -29,19 +30,44 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgresql://ffrkfknqbahajw:d84315a7a8fb07644553f98cbd4b1c36db4e4d7a8a78dbe7acc16f4cf8297944@ec2-3-225-213-67.compute-1.amazonaws.com:5432/dc3l910adjg8ve")
 db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 @login_required
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    pass
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+
+        # verificamos si el usuario nuevo ingreso en algo en los campos correspondientes
+        if not username:
+            return render_template("register.html")
+
+        elif not password:
+            return render_template("register.html")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=username)
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("login.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+    else:
+        render_template("login.html")
 
 @app.route("/logout")
 def logout():
@@ -54,6 +80,41 @@ def logout():
     return redirect("/login")
 
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register")
 def register():
-    pass
+
+    if request.method == "POST":
+        
+        username = request.form.get("username").strip()
+        password = request.form.get("password").strip()
+        confirmation = request.form.get("confirmation").strip()
+
+        # verificamos si el usuario nuevo ingreso en algo en los campos correspondientes
+        if not request.form.get("username"):
+            return render_template("register.html")
+
+        elif not request.form.get("password"):
+            return render_template("register.html")
+
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("register.html")
+
+
+        # Verificamos si el nombre del usuario esta disponible
+        if db.execute("SELECT username FROM users WHERE username = :username", username=username):
+            return render_template("register.html")
+
+        # insertamos al nuevo usuario
+        rows = db.execute("INSERT INTO users(username, hash) VALUES (:username, :hash)",
+                          username=username, hash=generate_password_hash(password))
+
+        # iniciamos session
+        session["user_id"] = rows
+
+        return redirect("/")
+    else:
+        render_template("register.html")
+
+@app.route("/error")
+def error():
+    return render_template("403.html")
